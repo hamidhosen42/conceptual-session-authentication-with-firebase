efa5ad0b0ef21b291db77a40fbcf2e9d91c9aa68
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2022-04-18 23:32:33 +0600

    Add files via upload

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAHRnDWhQ8tRq3dHOcNZ95wuit9vrH7bRI",
-    authDomain: "ranco-a0bb5.firebaseapp.com",
-    projectId: "ranco-a0bb5",
-    storageBucket: "ranco-a0bb5.appspot.com",
-    messagingSenderId: "775789481609",
-    appId: "1:775789481609:web:4b3555c113878daf944058",
+  apiKey: "AIzaSyCjeNQd-FOlL8TCxEMhg1o2Z_lmUBjn1lo",
+  authDomain: "react-color-generator-firebase.firebaseapp.com",
+  projectId: "react-color-generator-firebase",
+  storageBucket: "react-color-generator-firebase.appspot.com",
+  messagingSenderId: "522916022192",
+  appId: "1:522916022192:web:348db9a42fc69e73b1b875",
 };
 
 const app = initializeApp(firebaseConfig);
